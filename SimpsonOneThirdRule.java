b5091453042ad94d7ca7dde6fd7e2faf86df1fb1
mohdmehdi2021 <81911929+mohdmehdi2021@users.noreply.github.com>
2022-05-12 12:16:52 +0530

    Simpson 1/3rd rule
    Original Author = Mohd Mehdi

---
+++ SimpsonOneThirdRule.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class SOTR{
+    double y0,y_final,y1,y3,y5,y2,y4,b,a,no_ofSteps;
+    SOTR(double y0, double y_final,double y1, double y3,double y5,double y2,double y4,double lowerlim,double upperlim, double num_ofSteps){
+        this.y5=y5;
+        this.y3=y3;
+        this.y1=y1;
+        this.y_final=y_final;
+        this.y0=y0;
+        this.b=upperlim;
+        this.a = lowerlim;
+        this.no_ofSteps=num_ofSteps;
+        this.y2=y2;
+        this.y4=y4;
+    }
+
+    public double h(){
+        return (b-a)/no_ofSteps;
+    }
+
+    public double simpsonFormula(){
+        return ((h()/3)*((y0+y_final)+4*(y1+y3+y5)+2*(y2+y4)));
+    }
+}
+
+public class SimpsonOneThirdRule {
+    public static void main(String[] args) {
+        double lowlim,upperlim,steps,initValue,finValue,y1,y2,y3,y4,y5;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Lower Limit");
+        lowlim= scanner.nextDouble();
+        System.out.println("Enter Upper Limit");
+        upperlim= scanner.nextDouble();
+        System.out.println("Enter Number of Steps");
+        steps= scanner.nextDouble();
+        System.out.println("Enter initial value");
+        initValue= scanner.nextDouble();
+        System.out.println("Enter final value");
+        finValue= scanner.nextDouble();
+        System.out.println("Enter (Odd) Y1");
+        y1= scanner.nextDouble();
+        System.out.println("Enter (Odd) Y3");
+        y3= scanner.nextDouble();
+        System.out.println("Enter (Odd) Y5");
+        y5= scanner.nextDouble();
+        System.out.println("Enter (Even) Y2");
+        y2= scanner.nextDouble();
+        System.out.println("Enter (Even) Y4");
+        y4= scanner.nextDouble();
+        SOTR sotr = new SOTR(initValue,finValue,y1,y3,y5,y2,y4,lowlim,upperlim,steps);
+        System.out.println(sotr.simpsonFormula());
+    }
+}
